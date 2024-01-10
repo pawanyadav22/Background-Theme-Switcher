93cf99c4f5280c0b2cb3de9a61acbd63f3ade8ff
pawanyadav22 <102668510+pawanyadav22@users.noreply.github.com>
2024-01-10 16:16:40 +0530

    Add files via upload

---
+++ Background color changer/script.js
@@ -0,0 +1,15 @@
+const changeColorBtn = document.getElementById('changeColorBtn');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+changeColorBtn.addEventListener('click', () => {
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+});
